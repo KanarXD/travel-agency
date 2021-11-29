cef361d6a457eb6a4eb23eaed621a6227c0c3d03
KanarXD <piciolo@outlook.com>
2021-11-29 11:07:32 +0100

    add @Repository annotation

--- server-app/src/main/java/edu/put/serverapp/repositories/OfferRepository.java
+++ server-app/src/main/java/edu/put/serverapp/repositories/OfferRepository.java
@@ -2,6 +2,8 @@ package edu.put.serverapp.repositories;
 
 import edu.put.serverapp.models.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
 }
