373d5762aed90289ee38f8c629f07941a38feba2
KanarXD <piciolo@outlook.com>
2021-12-04 11:02:43 +0100

    modify app, app-routing modules

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppComponent } from './core/components/app/app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AppComponent} from './core/components/app/app.component';
+import {ContainersModule} from "./modules/modules.module";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
+    ContainersModule,
+    CoreModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
