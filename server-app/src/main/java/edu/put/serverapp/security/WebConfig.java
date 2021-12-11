d7e5d1911d3d531aa11075153258f34e59788f02
KanarXD <piciolo@outlook.com>
2021-12-11 19:24:25 +0100

    add cors config in application.yml

--- server-app/src/main/java/edu/put/serverapp/security/WebConfig.java
+++ server-app/src/main/java/edu/put/serverapp/security/WebConfig.java
@@ -1,16 +1,31 @@
 package edu.put.serverapp.security;
 
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Setter
 @EnableWebMvc
+@Configuration
+@ConfigurationProperties(prefix = "web-config")
 public class WebConfig implements WebMvcConfigurer {
+
+    private String frontAppUrl;
+
+    private String[] allowedMethodArray;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins(frontAppUrl)
+                .allowedMethods(allowedMethodArray);
     }
+
 }
+
