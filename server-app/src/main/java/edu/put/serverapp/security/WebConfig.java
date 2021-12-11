6f958a45b2cded0b432e319052e60cac9d73b2ea
KanarXD <piciolo@outlook.com>
2021-12-11 23:03:59 +0100

    refactor

--- server-app/src/main/java/edu/put/serverapp/security/WebConfig.java
+++ server-app/src/main/java/edu/put/serverapp/security/WebConfig.java
@@ -1,10 +1,7 @@
 package edu.put.serverapp.security;
 
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
