b42129b13789815523054d4222badf11827e36c5
KanarXD <piciolo@outlook.com>
2021-12-11 10:36:19 +0100

    add SERIAL ids, reservations refactor

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -6,14 +6,14 @@ GRANT ALL PRIVILEGES ON DATABASE travel_agency_db TO travel_agency;
 
 CREATE TABLE customers
 (
-    id      BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START 1 INCREMENT BY 1),
+    id      SERIAL PRIMARY KEY,
     name    VARCHAR(20) NOT NULL,
     surname VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE offers
 (
-    id         BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START 1 INCREMENT BY 1),
+    id         SERIAL PRIMARY KEY,
     name       VARCHAR(30)   NOT NULL,
     base_price NUMERIC(8, 2) NOT NULL,
     start_date DATE          NOT NULL,
@@ -22,9 +22,9 @@ CREATE TABLE offers
 
 CREATE TABLE reservations
 (
-    offer_id              BIGINT REFERENCES offers,
-    customer_id           BIGINT REFERENCES offers,
-    reservation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (offer_id, customer_id, reservation_timestamp)
+    id                    SERIAL PRIMARY KEY,
+    offer_id              INTEGER   NOT NULL REFERENCES offers,
+    customer_id           INTEGER   NOT NULL REFERENCES offers,
+    reservation_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
