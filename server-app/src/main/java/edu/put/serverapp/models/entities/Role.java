a787c12924c3ef7bddebf5d42448b6b942192dbd
KanarXD <piciolo@outlook.com>
2021-12-15 20:35:32 +0100

    add entities: privileges, roles, employees

---
+++ server-app/src/main/java/edu/put/serverapp/models/entities/Role.java
@@ -0,0 +1,24 @@
+package edu.put.serverapp.models.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+}
