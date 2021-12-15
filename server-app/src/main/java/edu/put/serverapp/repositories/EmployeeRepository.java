7763253bdebd80cebb1f7eb51a63acb4313a61d5
KanarXD <piciolo@outlook.com>
2021-12-15 20:38:02 +0100

    add repositories

---
+++ server-app/src/main/java/edu/put/serverapp/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package edu.put.serverapp.repositories;
+
+import edu.put.serverapp.models.entities.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Reservation, Integer> {
+}
