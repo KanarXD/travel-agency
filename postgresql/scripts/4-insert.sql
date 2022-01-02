0f66b33998ad41dacb5135870bb0062d52a13e1f
KanarXD <piciolo@outlook.com>
2022-01-02 13:06:56 +0100

    add offers references

--- postgresql/scripts/4-insert.sql
+++ postgresql/scripts/4-insert.sql
@@ -10,7 +10,7 @@ INSERT INTO customers(name, surname)
 VALUES ('ADAM', 'BEDNAREK');
 
 INSERT INTO hotels(name, location)
-VALUES ('Szeraton', 'Poznań');
+VALUES ('Sheraton', 'Poznań');
 
 INSERT INTO hotels(name, location)
 VALUES ('Gołębieski', 'Mikołajki');
@@ -33,14 +33,14 @@ VALUES ('ZŁA OFERTA', 22345.50, '2022-02-01', '2022-02-16', 1);
 INSERT INTO offers(name, base_price, start_date, end_date, carrier_id)
 VALUES ('NIEZŁA OFERTA', 235.50, '2022-01-03', '2022-01-06', 1);
 
-INSERT INTO offers(name, base_price, start_date, end_date)
-VALUES ('SUPER OFERTA', 2235.50, '2022-01-01', '2022-01-06');
+INSERT INTO offers(name, base_price, start_date, end_date, hotel_id, carrier_id)
+VALUES ('SUPER OFERTA', 2235.50, '2022-01-01', '2022-01-06', 2, 1);
 
-INSERT INTO offers(name, base_price, start_date, end_date)
-VALUES ('ZŁA OFERTA', 22345.50, '2022-02-01', '2022-02-16');
+INSERT INTO offers(name, base_price, start_date, end_date, hotel_id, carrier_id)
+VALUES ('ZŁA OFERTA', 22345.50, '2022-02-01', '2022-02-16', 3, 1);
 
-INSERT INTO offers(name, base_price, start_date, end_date)
-VALUES ('NIEZŁA OFERTA', 235.50, '2022-01-03', '2022-01-06');
+INSERT INTO offers(name, base_price, start_date, end_date, hotel_id, carrier_id)
+VALUES ('NIEZŁA OFERTA', 235.50, '2022-01-03', '2022-01-06', 3, 3);
 
 INSERT INTO employees(name, surname, login, password, role)
 VALUES ('BOB', 'BOBOWSKI', 'bob', 'bobspassword', (SELECT id FROM roles WHERE name = 'AGENT'));
