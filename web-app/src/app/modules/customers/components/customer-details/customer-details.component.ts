fa0b60c3987e9e0e4385339884ef1f40b924b7c3
KanarXD <piciolo@outlook.com>
2022-01-25 12:25:44 +0100

    refactor

--- web-app/src/app/modules/customers/components/customer-details/customer-details.component.ts
+++ web-app/src/app/modules/customers/components/customer-details/customer-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-customer-details',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
