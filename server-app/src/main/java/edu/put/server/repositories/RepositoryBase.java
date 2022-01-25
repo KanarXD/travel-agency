e53ff4ae7374be9ccf7df1e1a835fd90555344f4
KanarXD <piciolo@outlook.com>
2022-01-25 10:46:29 +0100

    fix RepositoryBase

--- server-app/src/main/java/edu/put/server/repositories/RepositoryBase.java
+++ server-app/src/main/java/edu/put/server/repositories/RepositoryBase.java
@@ -2,6 +2,8 @@ package edu.put.server.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface RepositoryBase<T> extends JpaRepository<T, Integer>, JpaSpecificationExecutor<T> {
 }
